691926215eb7ed82496773087454ee4c36a2150d
pgallino <pgallino@fi.uba.ar>
2025-06-16 14:28:07 -0300

    fix[US-11]: Cambio mensaje del bot al estar penalizado

--- app/constantes/mensajes.rb
+++ app/constantes/mensajes.rb
@@ -29,6 +29,6 @@ TEXT
 MENSAJE_MEDICO_YA_SELECCIONADO = 'Ya seleccionaste un médico, para agendar un nuevo turno envía el mensaje: /pedir-turno'.freeze
 MENSAJE_TURNO_YA_SELECCIONADO = 'Ya seleccionaste un turno, para seleccionar un nuevo médico envía el mensaje: /pedir-turno'.freeze
 TURNO_CANCELADO = '¡Uh! turno cancelado, esperamos que estes bien'.freeze
-MENSAJE_PENALIZACION = 'No puede reservar turnos por el momento debido a una penalización por bajo porcentaje de asistencia'.freeze
+MENSAJE_PENALIZACION = 'Usted se encuentra penalizado, tendrá que esperar para sacar un nuevo turno'.freeze
 MENSAJE_ESPECIALIDAD_YA_SELECCIONADA = 'Ya seleccionaste una especialidad, para agendar un nuevo turno envía el mensaje: /pedir-turno'.freeze
 MENSAJE_TIPO_DE_RESERVA_YA_SELECCIONADO = 'Ya seleccionaste un tipo de reserva, para agendar un nuevo turno envía el mensaje: /pedir-turno'.freeze
